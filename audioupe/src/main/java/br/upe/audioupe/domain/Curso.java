eb3a3799087013219bcce9fadfa4d6a064150fc5
Guiioff <guilhermerivaldy288@hotmail.com>
2024-02-12 19:26:20 -0300

    feat: criação do modelo de Curso

---
+++ audioupe/src/main/java/br/upe/audioupe/domain/Curso.java
@@ -0,0 +1,34 @@
+package br.upe.audioupe.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true)
+    private String nome;
+
+    private String descricao;
+
+    private String email;
+
+    private int horasAcs;
+
+    private String coordenacao;
+
+    @ManyToMany(mappedBy = "cursos")
+    private Set<Professor> professores;
+
+}
