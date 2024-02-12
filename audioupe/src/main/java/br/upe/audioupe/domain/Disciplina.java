e68975681b45e1bed9bcafcec2d27725c2953aa4
Guiioff <guilhermerivaldy288@hotmail.com>
2024-02-12 19:27:57 -0300

    feat: criação do modelo Disciplina

---
+++ audioupe/src/main/java/br/upe/audioupe/domain/Disciplina.java
@@ -0,0 +1,32 @@
+package br.upe.audioupe.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+
+    private int cargaHorariaTotal;
+
+    private String conteudoProgramatico;
+
+    private String materialApoio;
+
+    private String avaliacao;
+
+    @ManyToOne
+    @JoinColumn(name = "periodo_id")
+    private Periodo periodo;
+
+}
