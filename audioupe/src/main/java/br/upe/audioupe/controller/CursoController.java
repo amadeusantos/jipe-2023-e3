5db70e92739e9925e5ef7e21c2e6561c3d5ed549
Guiioff <guilhermerivaldy288@hotmail.com>
2024-02-13 13:20:48 -0300

    feat: criação da rota pata exibir todos os professores de um curso

--- audioupe/src/main/java/br/upe/audioupe/controller/CursoController.java
+++ audioupe/src/main/java/br/upe/audioupe/controller/CursoController.java
@@ -1,10 +1,12 @@
 package br.upe.audioupe.controller;
 
 import br.upe.audioupe.domain.dto.CursoDTO;
+import br.upe.audioupe.domain.dto.ProfessorDTO;
 import br.upe.audioupe.service.CursoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +19,15 @@ public class CursoController {
     @Autowired
     private CursoService cursoService;
 
+
     @GetMapping
     public ResponseEntity<List<CursoDTO>> todosCursos(){
         return ResponseEntity.ok().body(cursoService.exibirCursos());
+    }
+
+    @GetMapping("/{cursoId}/professores")
+    public ResponseEntity<List<ProfessorDTO>> professoresPorCurso(@PathVariable Long cursoId){
+        return ResponseEntity.ok().body(cursoService.professoresPorCurso(cursoId));
     }
 
 }
--- audioupe/src/main/java/br/upe/audioupe/service/CursoService.java
+++ audioupe/src/main/java/br/upe/audioupe/service/CursoService.java
@@ -2,8 +2,8 @@ package br.upe.audioupe.service;
 
 import br.upe.audioupe.domain.Curso;
 import br.upe.audioupe.domain.dto.CursoDTO;
+import br.upe.audioupe.domain.dto.ProfessorDTO;
 import br.upe.audioupe.repository.CursoRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +16,18 @@ public class CursoService {
     @Autowired
     private CursoRepository cursoRepository;
 
+    @Autowired
+    private ProfessorService professorService;
+
     public List<CursoDTO> exibirCursos(){
         List<Curso> cursos = cursoRepository.findAll();
         return cursos.stream()
                 .map(curso -> new CursoDTO(curso.getNome(), curso.getDescricao()))
                 .collect(Collectors.toList());
+    }
+
+    public List<ProfessorDTO> professoresPorCurso(Long cursoId){
+        return professorService.professoresPorCurso(cursoId);
     }
 
 }
