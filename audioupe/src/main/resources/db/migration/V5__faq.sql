f2edc910f5d951197c2319f9e5e5fa089810a5b8
Guilherme Rivaldy <Guilhermerivaldy288@hotmail.com>
2024-03-07 13:32:54 -0300

    feat: adição faq

---
+++ audioupe/src/main/resources/db/migration/V5__faq.sql
@@ -0,0 +1,6 @@
+INSERT INTO faq (id, nome, resposta) VALUES
+    (1, 'segunda chamada', 'A segunda chamada é uma oportunidade adicional para realizar uma avaliação ou exame após a chamada regular. Deve ser solicitada até 48 horas após a realização da avaliação '),
+    (2, 'matrícula', 'A matrícula é realizada sempre antes de todo inicio de um novo período. Confira o calendário acadêmico atual no site oficial da UPE e verifique o período de matrícula'),
+    (3, 'cancelamento de matrícula', 'O cancelamento de matrícula refere-se à ação de desistir ou interromper oficialmente a participação em um curso. Procure a escolaridade do campus para ter conhecimentos dos documentos necessários para esta ação'),
+    (4, 'localização', 'A UPE campus Garanhuns está localizada na Rua Capitão Pedro Rodrigues, no bairro São José em Garanhuns'),
+    (5, 'funcionamento', 'A UPE está aberta todos os dias úteis das 07 horas até as 22 horas');
